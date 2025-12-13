ab6d96e431e63daee0b52a33a28ed0b2ac9c5f49
𝘼𝙥𝙚𝙭𝘾𝙡𝙞𝙚𝙣𝙩 <its.go2225@gmail.com>
2025-12-13 17:31:31 +0300

    Init.lua

--- AdoptMe/Init.lua
+++ AdoptMe/Init.lua
@@ -1,1 +1,11 @@
+WindUI = getgenv().WindUI
+Window = getgenv().Window
 
+print('windows updated all success loaded man youre maked great work, i love you')
+
+if getgenv().Loaded ~= nil then
+	getgenv().Loaded = true
+	Notify('Successful hub loading!', 'GameID: ' .. game.PlaceId, 5)
+else
+	Window:Destroy()
+end
